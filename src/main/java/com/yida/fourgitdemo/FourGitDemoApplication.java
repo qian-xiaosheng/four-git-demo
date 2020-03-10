47c888b75b25f4ae4178bc26be1a735c0f51065e
qian-xiaosheng <1326598206@qq.com>
2020-03-10 12:25:35 +0800

    提交空的新工程

---
+++ src/main/java/com/yida/fourgitdemo/FourGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yida.fourgitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourGitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourGitDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yida/fourgitdemo/FourGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yida.fourgitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourGitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
